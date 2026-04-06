e0942611aa737b525155c17d2fa968899649a7f3
Nathene <95065326+Nathene@users.noreply.github.com>
2026-04-06 10:29:33 +1000

    Add: eating-bananas - submission-12

---
+++ Data Structures & Algorithms/eating-bananas/submission-12.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        auto can_eat = [&](int k) -> bool {
+            int hours = 0;
+            for (auto const& pile : piles) {
+                hours += (pile + k - 1) / k;
+            }
+            return (hours <= h);
+        };
+
+        int l = 1;
+        int r = *std::max_element(piles.begin(), piles.end());
+        int best = r;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (can_eat(mid)) {
+                best = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return best;
+    }
+
+};
