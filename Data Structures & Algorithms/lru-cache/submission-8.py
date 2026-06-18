978d05dc54bb248fcfb8cd649822279371fa64d0
Nathene <95065326+Nathene@users.noreply.github.com>
2026-06-18 13:21:37 +1000

    Add: lru-cache - submission-8

---
+++ Data Structures & Algorithms/lru-cache/submission-8.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.dummy_head = Node(0, 0)
+        self.dummy_tail = Node(0, 0)
+
+        self.dummy_head.next = self.dummy_tail
+        self.dummy_tail.prev = self.dummy_head
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    
+    def insert(self, node):
+        prev = self.dummy_tail.prev
+        nxt = self.dummy_tail
+    
+        prev.next = node
+        nxt.prev = node
+        
+        node.prev = prev
+        node.next = nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.dummy_head.next
+            self.remove(lru)
+            del self.cache[lru.key]
