dd7a5e9163521049e35b9d8cfb242c17c69468d1
Nathene <95065326+Nathene@users.noreply.github.com>
2026-06-25 16:17:39 +1000

    Add: dynamicArray - submission-7

---
+++ Data Structures & Algorithms/dynamicArray/submission-7.cpp
@@ -0,0 +1,60 @@
+class DynamicArray {
+    int capacity{};
+    int size{};
+    std::unique_ptr<int[]> arr;
+public:
+
+    DynamicArray(int capacity) : capacity(capacity), size(0) {
+        this->capacity = max(this->capacity, 1);
+        this-> arr = std::make_unique<int[]>(this->capacity);
+    }
+
+    int get(int i) {
+        if (i < 0 || i >= size) {
+            return -1;
+        }
+
+        return this->arr[i];
+    }
+
+    void set(int i, int n) {
+        if (i < 0 || i >= size) return;
+
+        this->arr[i] = n;
+    }
+
+    void pushback(int n) {
+        if (size == capacity) resize();
+
+        this->arr[size] = n;
+        size++;
+
+    }
+
+    int popback() {
+        if (size == 0) return -1;
+        size--;
+        int val = this->arr[size];
+        return val;
+    }
+
+    void resize() {
+        this->capacity *= 2;
+
+        std::unique_ptr<int[]> tmp = std::make_unique<int[]>(this->capacity);
+
+        for (int i{}; i < this->size; i++) {
+            tmp[i] = this->arr[i];
+        }
+        this->arr = std::move(tmp);
+    }
+
+    int getSize() {
+        return this->size;
+
+    }
+
+    int getCapacity() {
+        return this->capacity;
+    }
+};
